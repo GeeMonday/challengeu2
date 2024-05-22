5eedf214a8e4d30ccdccd84a1e1388455e7eaf57
GeeMonday <gmonday@academic.rrc.ca>
2024-05-21 21:49:43 -0500

    added new file

---
+++ 01-hello-world/hello_world.rb
@@ -0,0 +1,10 @@
+class HelloWorld
+    # Class-level method to return the greeting
+    def self.hello(name = nil)
+      if name
+        "Hello, #{name}!"
+      else
+        "Hello, World!"
+      end
+    end
+  end
