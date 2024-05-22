a5ed186704bbefd07906fd2c806002c5df883552
GeeMonday <gmonday@academic.rrc.ca>
2024-05-21 23:27:44 -0500

    updated pangram.rb

--- 04-pangram/pangram.rb
+++ 04-pangram/pangram.rb
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 class Pangram
     def self.is_pangram?(sentence)
       alphabet = ('a'..'z').to_a
       sentence.downcase.chars.uniq.select { |char| alphabet.include?(char) }.size == alphabet.size
     end
   end
+=======
+class Pangram
+    def self.is_pangram?(sentence)
+      alphabet = ('a'..'z').to_a
+      sentence.downcase.chars.uniq.select { |char| alphabet.include?(char) }.size == alphabet.size
+    end
+  end
+>>>>>>> 4847772 (updated pangram.rb)
